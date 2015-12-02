891c54f83bb986107c241e2f40ffc188cae7cb5d
Guilherme Souza <guivideojob@gmail.com>
2015-12-01 23:46:14 -0200

    It shall be named Robonaut

--- src/main/java/com/guilhermef/NasaRobot/servlets/SimpleServlet.java
+++ src/main/java/com/guilhermef/NasaRobot/servlets/SimpleServlet.java
@@ -8,13 +8,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.guilhermef.NasaRobot.OutOfBoundaries;
+import com.guilhermef.NasaRobot.Robonaut;
+
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
-		resp.setContentType("text/html");
-		out.println("Yo!");
+		String path = req.getPathInfo().replace("/", "");
+		Robonaut robot = new Robonaut();
+		try {
+			robot.move(path);
+			out.println(robot.getFinalPosition());
+		} catch (Exception e) {
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		}
 		out.close();
 	}
 }
